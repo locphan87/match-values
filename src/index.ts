da0b8b92a7cd8bbb747548c253bb0993dd42c8a6
Loc Phan <phanhoangloc@gmail.com>
2019-01-05 12:04:39 +0700

    fix: allow null/undefined as a match result

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,15 @@ interface IPattern {
 
 const match = (value: string, pattern: IPattern) => {
   const hasKey = (key: string) => String(value) === key
-  const matchingCase = Object.keys(pattern).find(hasKey) || '_'
-  const result = pattern[matchingCase]
+  const matchingCase = Object.keys(pattern).find(hasKey)
+  const defaultCase = '_'
+  const hasDefault = pattern.hasOwnProperty(defaultCase)
 
-  if (result === null || (typeof result === 'undefined')) {
-    throw new Error('Match error')
+  if (!matchingCase && !hasDefault) {
+    throw new Error(`Match error for value: ${value}`)
   }
 
-  return result
+  return pattern[matchingCase || defaultCase]
 }
 
 const lazyMatch = (pattern: IPattern) => (value: string) => match(value, pattern)
