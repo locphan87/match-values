29cd06aa52882b31af0e1363659499c9e414d9a3
Loc Phan <phanhoangloc@gmail.com>
2019-04-09 22:26:50 +0700

    chore: remove build assets

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-interface IPattern {
-    [branch: string]: any;
-}
-declare const match: (value: string, pattern: IPattern) => any;
-declare const matchArray: (value: any[], pattern: any[]) => any;
-declare const lazyMatch: (pattern: IPattern) => (value: string) => any;
-export { lazyMatch, matchArray };
-export default match;
